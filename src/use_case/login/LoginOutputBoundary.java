b655312f38111f6c83ab30939652953197261534
Krishna Lad <krishnalad54@gmail.com>
2023-12-04 16:04:42 -0500

    adressing some bugs

--- src/use_case/login/LoginOutputBoundary.java
+++
@@ -1,9 +0,0 @@
-package use_case.login;
-
-public interface LoginOutputBoundary {
-    void prepareSuccessView(LoginOutputData user);
-
-    void prepareFailViewUsername(String error);
-
-    void prepareFailViewPassword(String error);
-}
