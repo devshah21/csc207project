d8cf86945222da644000f195d7b8a2e93bad1739
Amaan Farooqi <amaan.farooqi2020@gmail.com>
2023-12-04 18:48:45 -0500

    commit

--- src/use_case/clear_users/ClearInputBoundary.java
+++
@@ -1,7 +0,0 @@
-package use_case.clear_users;
-import java.io.IOException;
-
-
-public interface ClearInputBoundary {
-    void execute() throws IOException;
-}
--- src/use_case/truefalse/TruefalseInputBoundary.java
+++
@@ -1,6 +0,0 @@
-package use_case.truefalse;
-
-public interface TruefalseInputBoundary {
-    void execute(TruefalseInputData inputData);
-}
-
